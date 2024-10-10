3560e40adc5374072103e167b7adb07ca870c238
Rutuparn <rutuparnthestoic@gmail.com>
2024-10-10 12:23:58 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//Rutuparn Kakade : rutuparnthestoic
+//Final project : Developing backend apps with Nodejs and Expressjs
+
+//Importing Modules
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const session = require('express-session')
+const customer_routes = require('./router/auth_users.js').authenticated;
+const genl_routes = require('./router/general.js').general;
+
+const app = express(); //App as express
+
+app.use(express.json()); //Using JSON
+
+app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+
+app.use("/customer/auth/*", function auth(req,res,next){
+//Write the authenication mechanism here
+});
+ 
+const PORT =5000;
+
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+app.listen(PORT,()=>console.log("Server is running"));
